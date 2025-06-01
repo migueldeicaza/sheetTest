f2309edd1c8e8dbe9f58ad7403703cfb6c25cc63
Miguel de Icaza <miguel@gnome.org>
2025-06-01 13:33:40 -0400

    Initial Commit

---
+++ sheetTest/sheetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheetTestApp.swift
+//  sheetTest
+//
+//  Created by Miguel de Icaza on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct sheetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sheetTestTests/sheetTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  sheetTestTests.swift
+//  sheetTestTests
+//
+//  Created by Miguel de Icaza on 6/1/25.
+//
+
+import Testing
+@testable import sheetTest
+
+struct sheetTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
